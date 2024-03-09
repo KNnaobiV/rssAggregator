d86810fb02688662e829cf49d14d115b286393b7
Nnaobi <nnaobi.godson@aiesec.net>
2024-03-09 14:04:34 +0100

    updated create user sql statement

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
 
-	"github.com/joho/godotenv"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
-)
+	"github.com/joho/godotenv"
+	"github.com/knnaobiv/rssaggregator/internal/database"
+	
+	_ "github.com/lib/pq"
+	)	
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
 
 func main() {
 	godotenv.Load()
@@ -17,6 +26,20 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil{
+		log.Fatal("Cannot connect to database:", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -31,11 +54,12 @@ func main() {
 			MaxAge: 300,
 		}),
 	)
-
+	
 	v1Router := chi.NewRouter()
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -45,8 +69,8 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
-	if err !=nil {
+	
+	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 
