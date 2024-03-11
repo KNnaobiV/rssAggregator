e4d2ee06d687f3c7fb76b7282d3419c260fa3e15
Nnaobi <nnaobi.godson@aiesec.net>
2024-03-11 17:58:38 +0100

    updated crud side

--- handler_user.go
+++ handler_user.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
-	"github.com/knnaobiv/rssaggregator/internal/database"
 	"github.com/google/uuid"
+	"github.com/knnaobiv/rssaggregator/internal/database"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -24,16 +24,21 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	}
 
 	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID: 		uuid.New(),
-		CreatedAt:	time.Now().UTC(),
-		UpdatedAt:	time.Now().UTC(),
-		Name:		params.Name,
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
 	})
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Could not create user %v", err))
 		return
 	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"database/sql"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/knnaobiv/rssaggregator/internal/database"
-	
+
 	_ "github.com/lib/pq"
-	)	
+)
 
 type apiConfig struct {
 	DB *database.Queries
 }
 
-
 func main() {
+	feed, err := urlToFeed(("https://wagslane.dev/index.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed)
+
+
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -34,7 +40,7 @@ func main() {
 	}
 
 	conn, err := sql.Open("postgres", dbURL)
-	if err != nil{
+	if err != nil {
 		log.Fatal("Cannot connect to database:", err)
 	}
 
@@ -46,30 +52,41 @@ func main() {
 
 	router.Use(
 		cors.Handler(cors.Options{
-			AllowedOrigins: []string{"https://*", "http://*"},
-			AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-			AllowedHeaders: []string{"*"},
-			ExposedHeaders: []string{"Link"},
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"*"},
+			ExposedHeaders:   []string{"Link"},
 			AllowCredentials: false,
-			MaxAge: 300,
+			MaxAge:           300,
 		}),
 	)
-	
+
 	v1Router := chi.NewRouter()
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerError)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
+	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
+	v1Router.Delete(
+		"/feed_follow/{feedFollowID}",
+		apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow),
+	)
 
 	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
-		Addr: ":" + portString,
+		Addr:    ":" + portString,
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	
+
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
